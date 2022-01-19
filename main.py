6b063cff78b555fe4d57a3f02d35d42a6e239ad9
Asep Sopiyan <asep.sopiyan1309@gmail.com>
2022-01-19 21:22:57 +0700

    finish real data from BMKG.go.id

--- main.py
+++ main.py
@@ -1,40 +1,9 @@
 """
 APLIKASI DETEKSI GEMPA TERKINI
 MODULARISASI DENGAN FUNCTION
+MODULARISASI DENGAN PACKAGE
 """
-
-
-def ekstrasi_data():
-    """
-    Tanggal: 17 Januari 2022
-    Waktu: 07:25:56 WIB
-    Magnitudo: 5.4
-    Kedalaman: 10 km
-    Lokasi: 7.60 LS - 105.90 BT
-    Pusat gempa: Pusat gempa berada di laut 84 km BaratDaya Bayah
-    Dirasakan: Dirasakan (Skala MMI): II-III Cikembar, II-III Cireunghas, Kab. Sukabumi, II-III Pelabuhanratu, II-III Sumur, II-III Bogor, III Bayah, III Pandeglang, III Cikeusik, III Panimbang, II-III Jakarta Barat, II-III Jakarta Selatan, II-III Tambun
-    :return:
-    """
-    hasil = dict()
-    hasil["tanggal"] = "17 Januari 2022"
-    hasil["waktu"] = "07:25:56 WIB"
-    hasil["magnitudo"] = 5.4
-    hasil["lokasi"] = {'ls': 7.60, 'bt': 105.90}
-    hasil["pusat"] = "Pusat gempa berada di laut 84 km BaratDaya Bayah"
-    hasil["dirasakan"] = "Dirasakan (Skala MMI): II-III Cikembar, II-III Cireunghas, Kab. Sukabumi, II-III Pelabuhanratu, II-III Sumur, II-III Bogor, III Bayah, III Pandeglang, III Cikeusik, III Panimbang, II-III Jakarta Barat, II-III Jakarta Selatan, II-III Tambun"
-
-    return hasil
-
-
-def tampilkan_data(result):
-    print("gempa terakhir berdasarkan BMKG")
-    print(f"Tanggal {result['tanggal']}")
-    print(f"Waktu {result['waktu']}")
-    print(f"Magnitudo {result['magnitudo']}")
-    print(f"Lokasi: LS={result['lokasi']['ls']}, BT={result['lokasi']['bt']}")
-    print(f"Pusat {result['pusat']}")
-    print(f"Dirasakan {result['dirasakan']}")
-
+from gempaterkini import ekstrasi_data,tampilkan_data
 
 if __name__ == '__main__':
     print('Aplikasi Utama')
